5b6e02aaca94f2eff60bf958d8f88f17817ac5d6
roanacla <roanacla@me.com>
2020-09-27 21:56:03 -0700

    Create iOS app

---
+++ iOS_Rekognition_Uploader/iOS_Rekognition_UploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Rekognition_UploaderApp.swift
+//  iOS_Rekognition_Uploader
+//
+//  Created by Roger Navarro on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Rekognition_UploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS_Rekognition_UploaderTests/iOS_Rekognition_UploaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_Rekognition_UploaderTests.swift
+//  iOS_Rekognition_UploaderTests
+//
+//  Created by Roger Navarro on 9/27/20.
+//
+
+import XCTest
+@testable import iOS_Rekognition_Uploader
+
+class iOS_Rekognition_UploaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
